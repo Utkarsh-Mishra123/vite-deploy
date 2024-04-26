7a971ba8d4dd134dee5630147c3ea76e455d6f18
Utkarsh Mishra <157680967+Utkarsh-Mishra123@users.noreply.github.com>
2024-04-27 00:56:16 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='bg-emerald-600 py-11 text-4xl text-white text-center'>How are you</h2>
+    </>
+  )
+}
+
+export default App
